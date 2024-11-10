0f4abaea72e38934013b98e98e0fc0bd44f302f7
iyasmzn <iyasmzn07@gmail.com>
2024-11-10 20:28:26 +0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,143 @@
+const logotext = "Iyas";
+const meta = {
+    title: "Iyas Muzani",
+    description: "I’m Iyas Muzani a Full stack developer, currently working in Semarang, Indonesia",
+};
+
+const introdata = {
+    title: "I’m Iyas Muzani",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Crafting seamless and dynamic web applications with expertise in both front-end and back-end technologies.",
+    your_img_url: "https://nuris-sdit-perpus.haunansite.com/assets/images/default/1731243132.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a passionate and dedicated Full Stack Developer, I specialize in creating dynamic and responsive web applications. With a strong foundation in both front-end and back-end technologies, I bring a holistic approach to web development, ensuring seamless integration and optimal performance.",
+};
+const worktimeline = [{
+        jobtitle: "Fullstack Mobile & Website Developer",
+        where: "PT. Mahir Techno",
+        date: "2019-2021",
+    },
+    {
+        jobtitle: "Fullstack Mobile & Website Developer",
+        where: "NH Press",
+        date: "2021-2022",
+    },
+    {
+        jobtitle: "Fullstack Website Developer",
+        where: "Trees4Trees",
+        date: "20022-Now",
+    },
+];
+
+const skills = [
+    {
+        name: "PHP",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "Vue",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 65,
+    },
+    {
+        name: "Wordpress",
+        value: 60,
+    },
+    {
+        name: "Figma",
+        value: 60,
+    },
+    {
+        name: "Solidity",
+        value: 30,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Provide application design with Figma.",
+    },
+    {
+        title: "Website",
+        description: "Build your dream website.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Build mobile application.",
+    },
+    {
+        title: "Wordpress",
+        description: "Make a website faster with wordpress.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://nuris-sdit-perpus.haunansite.com/assets/images/logo.gif",
+        description: "A book borrowing system for library management in elementary school.",
+        link: "https://sditnuris-perpus.haunansite.com/",
+    },
+    {
+        img: "https://luxeautohaus.com.au/wp-content/uploads/2023/11/cropped-thumbnail_LUXE-AUTOHAUS-Logo-black-1.png",
+        description: "Sydney's Leading Luxury Car Dealership.",
+        link: "https://luxeautohaus.com.au/",
+    },
+    {
+        img: "https://www.snoreexperts.com/img/SnoreExperts.svg",
+        description: "The website is intended for people who want to consult about problems in their sleep.",
+        link: "https://www.snoreexperts.com/",
+    },
+    {
+        img: "https://photos.app.goo.gl/ZLLRLPPorcjsNEKT9",
+        description: "Member data system for the Paskas Jogja organization.",
+        link: "https://app.paskasjogja.com/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Sulawesi wheelchair rental landing page.",
+        link: "https://fafemedika.com/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "iyasmzn07@gmail.com",
+    YOUR_FONE: "+6285175292107",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_ars5ylr",
+    YOUR_TEMPLATE_ID: "template_iko3mdl",
+    YOUR_USER_ID: "c9aLg_t36_XAcFhw_",
+};
+
+const socialprofils = {
+    github: "https://github.com/iyasmzn",
+    facebook: "https://facebook.com/iyasmzn",
+    // linkedin: "https://linkedin.com",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
